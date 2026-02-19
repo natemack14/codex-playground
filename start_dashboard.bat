84244681bfe3ab17312fd1daee165b120be4cc83
natemack14 <natemack@gmail.com>
2026-02-19 12:57:31 -0500

    start_dashboard.bat

--- start_dashboard.bat
+++ start_dashboard.bat
@@ -1,6 +1,4 @@
 @echo off
 cd /d "%~dp0"
-python -m streamlit run app.py --server.headless true --browser.gatherUsageStats false
-
-
-
+start "" http://localhost:8507
+python -m streamlit run app.py --server.port 8507 --server.headless true
